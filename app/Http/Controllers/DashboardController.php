71d0d5b47a963a02944a15c1caec187e245c18da
Benoit.Spriet <benoit.spriet@epitech.eu>
2021-08-09 17:13:08 +0200

    count_vote_topic ok

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -12,38 +12,23 @@ class DashboardController extends BaseController
 {
     public function count_vote_topic() {
 
-        $array = [];
+        $labels = [];
+        $data = [];
         $start = Carbon::now()->subMonth()->format('Y-m-d H:m:s');
         $end = Carbon::now()->format('Y-m-d H:m:s');
 
         $period = CarbonPeriod::create($start, '1 day', $end);
-            foreach ($period as $date) {
-                array_push($array, $date);
-            }
+        foreach ($period as $date) {
+            $date_format = $date->format('Y-m-d');
+            $count_vote = VoteTopic::where('created_at','LIKE', "$date_format%")->count();
+            array_push($data, $count_vote);
+            array_push($labels, $date->format('D'));
+        }
 
-        // for ($index=$start; $index->ad)
-
-        // $test = VoteTopic::where('created_at', '=', $date)->count();
+        $chartData = ['labels' => $labels, 'data' => $data];
 
 
-
-        return $this->sendResponse($array,"ok",200);
+        return $this->sendResponse($chartData,"ok",200);
 
     }
 }
-
-// data() {
-//     return {
-//       chartData: {
-//         labels: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Mo', 'Tu', 'We'],
-//         datasets: [
-//           {
-//             label: 'Line Chart',
-//             data: [5600, 4150, 5420, 4050, 6522, 7241, 5259, 6157, 7545, 7841],
-//             fill: false,
-//             borderColor: '#2554FF',
-//             backgroundColor: '#2554FF',
-//             borderWidth: 1,
-//           },
-//         ],
-//       },
