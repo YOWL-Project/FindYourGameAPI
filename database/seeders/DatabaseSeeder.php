d4d68944856778fd669f7f8bd3de693e5a292aab
Benoit.Spriet <benoit.spriet@epitech.eu>
2021-08-09 14:13:05 +0200

    seeder ok

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(40)->create();
+        \App\Models\Topic::factory(35)->create();
+        \App\Models\VoteTopic::factory(100)->create();
     }
 }
